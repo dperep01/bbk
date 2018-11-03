c78fdfbf93b5cd25fa84f206f5d3cdbe08fab21a
xren <xren@xrens.local>
2018-11-03 11:05:58 +0000

    1

--- utl.py
+++ utl.py
@@ -23,9 +23,9 @@ def calc_T(c1, c2):
     else:
         return y*2*len(CONTRACTS) - CONTRACTS[l1] - (len(CONTRACTS) - CONTRACTS[l2])
 
-def calc_expiry(mkt='SCF.csv', save=False):
+def gen_expiry(mkt='SCF', save=False):
 #   pass in file name with raw Reuters data for individual contracts in one csv file 
-    fn = mkt
+    fn = mkt+'.csv'
     df = pd.read_csv(fn, parse_dates=['Trade Date'], infer_datetime_format=True)
     df = df[['RIC', 'Trade Date', 'Universal Close Price']].dropna()
     df.rename(columns={'Trade Date':'Date', 'Universal Close Price':'Close'}, inplace=True)
@@ -38,19 +38,45 @@ def calc_expiry(mkt='SCF.csv', save=False):
         exp['CRIC'] = exp.Exp.dt.year.astype(str) + exp.RIC.str[3:4]
         exp = exp[['RIC', 'CRIC', 'Exp']]
         expiry = expiry.append(exp)
-
     if save:
-        df.to_csv(mkt+'-with-expiry.csv', index=False)
-    return expiry
+        fn = mkt+'-expiry.csv'
+        df.to_csv(fn, index=False)
+        print('Saved expiry data to %s'%fn)
+    else:
+        return expiry
 
-#import utl
-#idf = pd.read_csv('SCF-with-expiry.csv', index_col=1)
-#mx = pd.DataFrame(columns=['Date']+list(range(12)))
-#i = 0
-#for n,g in df.groupby('Date'):
-#    v = g.sort_values('CRIC')
-#    v['TT'] = utl.calc_TT(v.CRIC.tolist())
-#    v = v.set_index('TT').T
-#    v['Date'] = n
-#    mx = mx.append(v['Close':'Close'])
-#print(mx)
+def add_expiry(mkt='SCF', save=False):
+    fn = mkt+'.csv'
+    df = pd.read_csv(fn, parse_dates=['Trade Date'], infer_datetime_format=True)
+    df = df[['RIC', 'Trade Date', 'Universal Close Price']].dropna()
+    df.rename(columns={'Trade Date':'Date', 'Universal Close Price':'Close'}, inplace=True)
+
+    expiry = gen_expiry(mkt)
+    df['Exp'] = np.nan; df['CRIC'] = np.nan
+
+    for index, row in expiry.sort_values('Exp', ascending=False).iterrows():
+        idx = (df.RIC==row.RIC) & (df.Date <= row.Exp)
+        df.loc[idx, ['Exp', 'CRIC']] = [row.Exp, row.CRIC]
+    if save:
+        fn = mkt+'-with-expiry.csv'
+        df.to_csv(fn, index=False)
+        print('Saved data with added expiry to %s'%fn)
+    else:
+        return df
+
+def gen_fc_3d(mkt, save=False):
+    df = pd.read_csv(mkt+'-with-expiry.csv', index_col=1)
+    mx = pd.DataFrame(columns=['Date']+list(range(12)))
+
+    for n,g in df.groupby('Date'):
+        v = g.sort_values('CRIC')
+        v['TT'] = calc_TT(v.CRIC.tolist())
+        v = v.set_index('TT').T
+        v['Date'] = n
+        mx = mx.append(v['Close':'Close'])
+    if save:
+        fn = mkt+'-fc-3d.csv'
+        mx.to_csv(fn, index=False)
+        print('Saved 3d forward curve to %s'%fn)
+    else:
+        return mx
